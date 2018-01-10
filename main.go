a6f9dbb0d9ac452bb9e072512e7c202b4a449491
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-01-10 14:15:49 +0900

    bump

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "ft"
 	app.Usage = "File Transferer"
-	app.Version = "0.0.1"
+	app.Version = "0.0.2"
 	app.Commands = []cli.Command{
 		downloadCommand(),
 		serveCommand(),
